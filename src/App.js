21e495afa03d05ba6164dde34a35c7b72bce34f3
Hy <hyhuynh222@gmail.com>
2023-11-25 18:57:14 -0800

    finalized

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { marked } from 'marked';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { marked } from "marked";
+import "./App.css";
 
 // allows break between the sentences
 marked.use({
-  breaks: true
+  breaks: true,
 });
 
 function App() {
   const [markdown, setMarkdown] = useState(
-// Make sure the initial state is set to be all the way to the left
+    // Make sure the initial state is set to be all the way to the left
 `# Welcome to my React Markdown Previewer!
+
 ## This is a sub-heading...
 ### And here's some other cool stuff:
 
@@ -20,9 +21,9 @@ Heres some code, \`<div></div>\`, between 2 backticks.
 // this is multi-line code:
 
 function anotherExample(firstLine, lastLine) {
-    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
     return multiLineCode;
-    }
+  }
 }
 \`\`\`
 
@@ -36,14 +37,14 @@ There's also [links](https://www.freecodecamp.org), and
 
 And if you want to get really crazy, even tables:
 
-Wild Header      | Crazy Header    | Another Header?
------------------| ----------------| -------------
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
 Your content can | be here, and it | can be here....
-And here.        | Okay.           | I think we get it.
+And here. | Okay. | I think we get it.
 
 - And of course there are lists.
-    - Some are bulleted.
-        - With different indentation levels.
+  - Some are bulleted.
+     - With different indentation levels.
         - That look like this.
 
 
@@ -51,7 +52,8 @@ And here.        | Okay.           | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)`    
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`
   );
 
   // inital load (componentDidMount() essentially)
@@ -60,23 +62,33 @@ And here.        | Okay.           | I think we get it.
   }, [markdown]);
 
   //handel when the text in the box is changed
-  const handleChange = event => {
+  const handleChange = (event) => {
     setMarkdown(marked.parse(event.target.value));
-  }
+  };
 
-
- 
   return (
-    <div>
-
-      <div><textarea onChange={handleChange}>{markdown}</textarea></div>
-      <div>
-        <div id="preview" dangerouslySetInnerHTML={{ __html: markdown }}></div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-4">
+          <div id="editor-heading">
+            <h2 id="sub-heading">Editor</h2>
+            <textarea id="editor" class="" onChange={handleChange}>
+              {markdown}
+            </textarea>
+          </div>
+        </div>
+        <div className="col-8">
+            <div id="preview-heading">
+              <h2 id="sub-heading">Preview</h2>
+              <div
+                id="preview"
+                dangerouslySetInnerHTML={{ __html: markdown }}
+              ></div>
+            </div>
+          </div>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
